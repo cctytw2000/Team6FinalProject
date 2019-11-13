42e00cec78016f3eba958d8c075b155500b8fbf1
unknown <cctytw2000@gmail.com>
2019-11-13 13:32:07 +0800

    john201911131331
    圖表版面

--- Team6FinalProject/src/main/webapp/WEB-INF/views/JS/membersBack.js
+++ Team6FinalProject/src/main/webapp/WEB-INF/views/JS/membersBack.js
@@ -123,18 +123,8 @@ $(document).ready(function() {
 					datasets : [ {
 						label : '每日登入帳號人數',
 						data : count,
-						backgroundColor : [ 'rgba(255, 99, 132, 0.2)',
-								'rgba(54, 162, 235, 0.2)',
-								'rgba(255, 206, 86, 0.2)',
-								'rgba(75, 192, 192, 0.2)',
-								'rgba(153, 102, 255, 0.2)',
-								'rgba(255, 159, 64, 0.2)' ],
-						borderColor : [ 'rgba(255,99,132,1)',
-								'rgba(54, 162, 235, 1)',
-								'rgba(255, 206, 86, 1)',
-								'rgba(75, 192, 192, 1)',
-								'rgba(153, 102, 255, 1)',
-								'rgba(255, 159, 64, 1)' ],
+						backgroundColor : 'lightgreen',
+						borderColor : 'brack',
 						borderWidth : 1
 					} ]
 				}
