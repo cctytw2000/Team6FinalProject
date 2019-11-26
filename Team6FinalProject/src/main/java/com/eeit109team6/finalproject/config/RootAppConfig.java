4676703037a73d1b41aed89f662baa1bb5cd1f05
Andy <andy41003a029@gmail.com>
2019-11-26 14:58:10 +0800

    andy201911261457
    註解hibernate語法

--- Team6FinalProject/src/main/java/com/eeit109team6/finalproject/config/RootAppConfig.java
+++ Team6FinalProject/src/main/java/com/eeit109team6/finalproject/config/RootAppConfig.java
@@ -53,8 +53,8 @@ public class RootAppConfig {
 	private Properties additionalProperties() {
 		Properties properties = new Properties();
 		properties.put("hibernate.dialect", org.hibernate.dialect.SQLServer2012Dialect.class);
-		properties.put("hibernate.show_sql", Boolean.TRUE);
-		properties.put("hibernate.format_sql", Boolean.TRUE);
+//		properties.put("hibernate.show_sql", Boolean.TRUE);
+//		properties.put("hibernate.format_sql", Boolean.TRUE);
 		properties.put("default_batch_fetch_size", 10);
 //		properties.put("hibernate.hbm2ddl.auto", "update");
 		return properties;
